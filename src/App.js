406b09f7b7eecbb2ae281c3e1a942e52099e06f3
github_username <you@domain.com>
2021-10-05 11:58:01 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <h1>Hello from tech Store</h1>;
+}
+
+export default App;
